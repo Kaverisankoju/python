308e989766a62791dbed92b6867f6ac6bb9de665
Kaverisankoju <158299969+Kaverisankoju@users.noreply.github.com>
2025-08-20 11:25:20 +0530

    Delete temp.py

--- temp.py
+++
@@ -1,9 +0,0 @@
-# Input: Temperature in Celsius
-celsius = float(input("Enter temperature in Celsius: "))
-
-# Convert to Fahrenheit using the formula
-fahrenheit = celsius * 9/5 + 32
-
-# Print both values
-print("Temperature in Celsius:", celsius)
-print("Temperature in Fahrenheit:", fahrenheit)
